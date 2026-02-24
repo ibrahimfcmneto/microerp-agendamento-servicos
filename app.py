cb4a22958dadd8a498aca57c8574c78748e4b507
ibrahimfcmneto <ibrahim.fcmn@puccampinas.edu.br>
2026-02-24 17:06:08 -0300

    Melhorias

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from models import db, login_manager, bcrypt, Client 
 from routes import init_routes
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -14,33 +15,44 @@ def create_app():
     
     login_manager.login_view = 'login' 
     login_manager.login_message = "Por favor, faça login para acessar esta página."
+    login_manager.login_message_category = "info"
 
     init_routes(app)
 
-    # --- CORREÇÃO: FORÇAR A CRIAÇÃO OU ATUALIZAÇÃO DO ADMIN ---
+    # --- CONFIGURAÇÃO AUTOMÁTICA DO BANCO E ADMIN ---
     with app.app_context():
-        db.create_all()
-
-        admin_email = "admin@barbearia.com"
-        senha_padrao = "admin123"
+        # db.drop_all() 
         
-        # Procura se o admin já existe
-        admin = Client.query.filter_by(email=admin_email).first()
+        # Garante que as tabelas existam sem apagar os dados atuais
+        db.create_all() 
 
-        if not admin:
-            # Se não existe, cria do zero
-            print("⚠️ Admin não encontrado. Criando novo...")
-            admin = Client(name="Gestor", email=admin_email, phone="000000000")
-            admin.set_password(senha_padrao)
-            db.session.add(admin)
-            print("✅ Conta criada!")
-        else:
-            # Se JÁ existe, ATUALIZA a senha para garantir que está certa
-            print("⚠️ Admin já existe. Atualizando senha para o padrão...")
-            admin.set_password(senha_padrao)
-            print("✅ Senha atualizada!")
+        admin_email = app.config.get('ADMIN_EMAIL')
+        senha_padrao = app.config.get('ADMIN_PASSWORD')
+        
+        if not admin_email or not senha_padrao:
+            print("❌ ERRO: ADMIN_EMAIL ou ADMIN_PASSWORD não encontrados no .env")
+            return app
 
-        db.session.commit()
+        try:
+            admin = Client.query.filter_by(email=admin_email).first()
+
+            if not admin:
+                print(f"⚠️ Criando novo Admin: {admin_email}")
+                admin = Client(
+                    name="Gestor", 
+                    email=admin_email, 
+                    phone="000000000",
+                    role="barber" 
+                )
+                admin.set_password(senha_padrao)
+                db.session.add(admin)
+                db.session.commit()
+                print("✅ Conta do administrador criada com sucesso!")
+            # else:
+            #    print("ℹ️ Admin já existe no banco.")
+
+        except Exception as e:
+            print(f"❌ Erro crítico: {e}")
 
     return app
 
